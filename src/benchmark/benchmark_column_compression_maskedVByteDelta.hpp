97adceef59e38d7a96aed16a30bd43bceedac07d
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com"$REMOTE" -ancestor "$BASE" -merge "$MERGED>
2020-11-30 11:43:39 +0100

    implements point access benchmark for maskedVByteDelta

--- src/benchmark/benchmark_column_compression_maskedVByteDelta.hpp
+++ src/benchmark/benchmark_column_compression_maskedVByteDelta.hpp
@@ -43,6 +43,32 @@ void maskedVByteDelta_benchmark_decoding(const std::vector<ValueT>& vec, benchma
   }
 }
 
+void maskedVByteDelta_benchmark_decoding_points(const std::vector<ValueT>& vec, const std::vector<size_t>& pointIndices, benchmark::State& state) {
+  // Encode
+  int N = vec.size();
+  ValueT* datain = new ValueT[N];
+  std::copy(vec.begin(), vec.end(), datain);
+  uint8_t* compressedbuffer = (uint8_t*)malloc(N * sizeof(ValueT));
+  vbyte_encode_delta(datain, vec.size(), compressedbuffer, 0);
+
+  // Decode
+  ValueT* data_recovered = new ValueT[N];
+  benchmark::DoNotOptimize(data_recovered);
+
+  std::vector<ValueT> points {};
+  points.resize(pointIndices.size());
+  benchmark::DoNotOptimize(points);
+
+  for (auto _ : state) {
+    masked_vbyte_decode_delta(compressedbuffer, data_recovered, N, 0);
+    for (size_t i = 0; i < pointIndices.size(); i++) {
+      points[i] = data_recovered[pointIndices[i]];
+    }
+
+    benchmark::ClobberMemory();
+  }
+}
+
 float maskedVByteDelta_compute_bitsPerInt(std::vector<ValueT>& vec) {
   // Encode
   int N = vec.size();
