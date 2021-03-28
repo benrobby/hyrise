7d6b67290839ad73ab9b4f3a3a4d5231d9fb55ef
Ben Hurdelhey <26392770+ben243871694@users.noreply.github.com>
2021-03-28 23:33:55 +0200

    fix push_back

--- src/lib/storage/vector_compression/bitpacking/bitpacking_compressor.cpp
+++ src/lib/storage/vector_compression/bitpacking/bitpacking_compressor.cpp
@@ -18,7 +18,7 @@ std::unique_ptr<const BaseCompressedVector> BitpackingCompressor::compress(
   auto data = pmr_bitpacking_vector<uint32_t, 16>(alloc);
   data.resize(vector.size());
   for (int i = 0; i < vector.size(); i++) {
-    data.push_back(vector[i]);
+    data[i] = vector[i];
   }
   // data.resize(data.size());
 
